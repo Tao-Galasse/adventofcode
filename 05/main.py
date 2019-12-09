651afca8c990a58bfcc7b2875aaa007df72d8e0b
Tao <tao.galasse@gmail.com>
2019-12-09 15:26:17 +0100

    day 5 - second part (Python)

--- 05/main.py
+++ 05/main.py
@@ -9,9 +9,15 @@ def set_intcode():
 
 def decrypt_instruction(intcode, index):
     instructions = "{0:0=5d}".format(intcode[index])
-    opcode = instructions[-2:]
-    address1 = index+1 if int(instructions[2]) else intcode[index+1]
-    address2 = index+2 if int(instructions[1]) else intcode[index+2]
+
+    opcode = instructions[-2:]  
+    address1 = address2 = 0 # defaut value
+
+    # Following instructions avoid a `list index out of range` error
+    if len(intcode) > index+1:
+        address1 = index+1 if int(instructions[2]) else intcode[index+1]
+    if len(intcode) > index+2:
+        address2 = index+2 if int(instructions[1]) else intcode[index+2]
 
     return (opcode, address1, address2)
 
@@ -33,6 +39,16 @@ def compute(intcode):
         elif opcode == "04":
             print(intcode[address1])
             index += 2
+        elif opcode == "05":
+            index = intcode[address2] if intcode[address1] != 0 else index+3
+        elif opcode == "06":
+            index = intcode[address2] if intcode[address1] == 0 else index+3
+        elif opcode == "07":
+            intcode[intcode[index+3]] = 1 if intcode[address1] < intcode[address2] else 0
+            index += 4
+        elif opcode == "08":
+            intcode[intcode[index+3]] = 1 if intcode[address1] == intcode[address2] else 0
+            index += 4
         elif opcode == "99":
             break
         else:
