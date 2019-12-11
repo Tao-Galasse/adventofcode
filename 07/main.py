25664f05b8f24ebd2f5a139d667339351d519e6d
Tao <tao.galasse@gmail.com>
2019-12-11 01:02:35 +0100

    day 7 - second part (Python) - refactor compute method & create an Amplifier class

--- 07/main.py
+++ 07/main.py
@@ -23,51 +23,76 @@ def decrypt_instruction(intcode, index):
 
     return (opcode, address1, address2)
 
-def compute(intcode, input1, input2, mode = "standard"):
-    index = 0
+class Amplifier:
+    def __init__(self, intcode, setting = None):
+        self.intcode = intcode
+        self.index = 0
+        self.setting = setting
+        self.setting_used = 0
+        self.output = None
+        self.halted = 0
 
-    while index < len(intcode):
-        opcode, address1, address2 = decrypt_instruction(intcode, index)
+    def compute(self, previous_output = None, mode = "standard"):
+        intcode = self.intcode
+        index = self.index
 
-        if opcode == "01":
-            intcode[intcode[index+3]] = intcode[address1] + intcode[address2]
-            index += 4
-        elif opcode == "02":
-            intcode[intcode[index+3]] = intcode[address1] * intcode[address2]
-            index += 4
-        elif opcode == "03":
-            intcode[intcode[index+1]] = input1 if input1 is not None else input2
-            input1 = None
-            index += 2
-        elif opcode == "04":
-            if mode == "standard":
-                print(intcode[address1])
-            elif mode == "output":
-                return intcode[address1]
-            index += 2
-        elif opcode == "05":
-            index = intcode[address2] if intcode[address1] != 0 else index+3
-        elif opcode == "06":
-            index = intcode[address2] if intcode[address1] == 0 else index+3
-        elif opcode == "07":
-            intcode[intcode[index+3]] = 1 if intcode[address1] < intcode[address2] else 0
-            index += 4
-        elif opcode == "08":
-            intcode[intcode[index+3]] = 1 if intcode[address1] == intcode[address2] else 0
-            index += 4
-        elif opcode == "99":
-            break
-        else:
-            print("wtf?")
+        while index < len(intcode):
+            opcode, address1, address2 = decrypt_instruction(intcode, index)
+
+            if opcode == "01":
+                intcode[intcode[index+3]] = intcode[address1] + intcode[address2]
+                index += 4
+            elif opcode == "02":
+                intcode[intcode[index+3]] = intcode[address1] * intcode[address2]
+                index += 4
+            elif opcode == "03":
+                if mode == "standard":
+                    intcode[intcode[index+1]] = input("Please provide the system ID:")
+                elif mode == "output":
+                    if not self.setting_used:
+                        intcode[intcode[index+1]] = self.setting
+                        self.setting_used = 1
+                    else:
+                        self.intcode[intcode[index+1]] = previous_output
+                index += 2
+            elif opcode == "04":
+                index += 2
+                if mode == "standard":
+                    print(intcode[address1])
+                elif mode == "output":
+                    self.output = intcode[address1]
+                    self.intcode = intcode
+                    self.index = index
+                    return self.output
+            elif opcode == "05":
+                index = intcode[address2] if intcode[address1] != 0 else index+3
+            elif opcode == "06":
+                index = intcode[address2] if intcode[address1] == 0 else index+3
+            elif opcode == "07":
+                intcode[intcode[index+3]] = 1 if intcode[address1] < intcode[address2] else 0
+                index += 4
+            elif opcode == "08":
+                intcode[intcode[index+3]] = 1 if intcode[address1] == intcode[address2] else 0
+                index += 4
+            elif opcode == "99":
+                if mode == "standard":
+                    break
+                elif mode == "output":
+                    self.halted = 1
+                    self.intcode = intcode
+                    self.index = index
+                    return self.output
+            else:
+                print("wtf?")
 
 def final_amplifier_output(settings_sequence):
-    amplifier_output = 0
+    output = 0
 
     for setting in settings_sequence:
-        intcode = set_intcode()
-        amplifier_output = compute(intcode, setting, amplifier_output, "output")
+        amplifier = Amplifier(set_intcode(), setting)
+        output = amplifier.compute(output, "output")
 
-    return amplifier_output
+    return output
 
 def get_max_thruster_signal():
     settings_sequences = list(itertools.permutations([0, 1, 2, 3, 4]))
@@ -79,4 +104,27 @@ def get_max_thruster_signal():
 
     return max
 
-print(get_max_thruster_signal())
+def get_max_thruster_signal_fallback_loop():
+    settings_sequences = list(itertools.permutations([5, 6, 7, 8, 9]))
+    max = 0
+
+    for settings_sequence in settings_sequences:
+        output = 0
+        amplifiers = []
+
+        for setting in settings_sequence:
+            amplifiers.append(Amplifier(set_intcode(), setting))
+        
+        while not amplifiers[-1].halted:
+            for amplifier in amplifiers:
+                output = amplifier.compute(output, "output")
+
+        if output > max: max = output
+
+    return max
+
+max_part1 = get_max_thruster_signal()
+max_part2 = get_max_thruster_signal_fallback_loop()
+
+print("Max thruster signal without fallback loop is %s" % max_part1)
+print("Max thruster signal with fallback loop is %s" % max_part2)
