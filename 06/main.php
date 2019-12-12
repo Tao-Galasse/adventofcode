5e2bde3673f8d0513e122dff51abd94e9e8b8f15
Tao <tao.galasse@gmail.com>
2019-12-12 22:55:29 +0100

    day 6 - second part (PHP)

--- 06/main.php
+++ 06/main.php
@@ -1,9 +1,10 @@
 <?php
+	# PART 1
+	#
 	function getOrbits(): array {
         $file = file("input.txt");
-        $orbits = [];
 
-        foreach($file as $line) {
+        foreach ($file as $line) {
             $line = explode(')', trim($line));
             $orbits[$line[1]] = $line[0];
         }
@@ -11,7 +12,8 @@
         return $orbits;
     }
 
-    function countOrbits(array $orbits): int {
+    function countOrbits(): int {
+    	global $orbits;
         $total = 0;
 
         foreach ($orbits as $satellite => $planet) {
@@ -26,7 +28,69 @@
     }
 
     $orbits = getOrbits();
-    $total = countOrbits($orbits);
+    $total = countOrbits();
 
     echo "Total number of direct and indirect orbits: $total\n";
+
+    # PART 2
+    #
+    function getOrbitedBy(): array {
+        $file = file("input.txt");
+
+        foreach ($file as $line) {
+            $line = explode(')', trim($line));
+            $orbitedBy[$line[0]][] = $line[1];
+        }
+
+        return $orbitedBy;
+    }
+
+    function getNextPossiblePlanets(string $planet = null, array $visitedPlanets): array {
+    	global $orbits, $orbitedBy;
+
+    	$nextPossiblePlanets = [];
+	    
+	    if (isset($orbits[$planet]) && !in_array($orbits[$planet], $visitedPlanets)) {
+	        array_push($nextPossiblePlanets, $orbits[$planet]);
+	    }
+
+	    if (is_array($orbitedBy[$planet])) {
+		    foreach ($orbitedBy[$planet] as $index => $satellite) {
+		        if (!in_array($satellite, $visitedPlanets)) {
+		            array_push($nextPossiblePlanets, $satellite);
+		        }
+		    }
+		}
+
+	    return $nextPossiblePlanets;
+    }
+
+    function findShortestPath(string $from, string $to): int {
+    	global $orbits, $orbitedBy;
+
+    	$path = [ [$orbits[$from], 0] ];
+    	$length = 0;
+    	$visitedPlanets = [$from];
+
+    	while (true) {
+    		[$planet, $length] = array_shift($path);
+
+    		$nextPossiblePlanets = getNextPossiblePlanets($planet, $visitedPlanets);
+
+    		foreach ($nextPossiblePlanets as $nextPlanet) {
+		        if ($nextPlanet === $orbits[$to]) {
+		            $length++;
+		            return $length;
+		        }
+		        array_push($path, [$nextPlanet, $length+1]);
+		    }
+
+		    array_push($visitedPlanets, $planet);
+		}
+    }
+
+	$orbitedBy = getOrbitedBy();
+    $transfers = findShortestPath("YOU", "SAN");
+
+	echo "Minimum number of orbital transfers required: $transfers\n";
 ?>
