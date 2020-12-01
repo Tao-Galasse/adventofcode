0cf7fd8f310cbc0b7411d1273a6ce7e4e50de641
Tao <tao.galasse@gmail.com>
2020-12-01 12:48:23 +0100

    2020: day 1

---
+++ 2020/01/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Return values from input file as an array of integers
+def expenses():
+    expenses = []
+
+    with open("input.txt", "r") as file:
+        for expense in file.read().splitlines():
+            expenses.append(int(expense))
+
+    return expenses
+
+
+# Find the two values giving a sum equal to 2020 from report
+def find_sum_of_two_values(report):
+    for i in range(0, len(report)):
+        for j in range(i+1, len(report)):
+            if report[i] + report[j] == 2020:
+                return (report[i], report[j])
+
+
+# Find the three values giving a sum equal to 2020 from report
+def find_sum_of_three_values(report):
+    for i in range(0, len(report)):
+        for j in range(i+1, len(report)):
+            for k in range(j+1, len(report)):
+                if report[i] + report[j] + report[k] == 2020:
+                    return (report[i], report[j], report[k])
+
+
+# Puzzle 1
+expense_report = expenses()
+expense1, expense2 = find_sum_of_two_values(expense_report)
+print("Puzzle 1 answer is:", expense1 * expense2)
+
+# Puzzle 2
+expense1, expense2, expense3 = find_sum_of_three_values(expense_report)
+print("Puzzle 2 answer is:", expense1 * expense2 * expense3)
---
+++ 2020/README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2020
+
+#### Advent of Code 2020 in Python 🐍🎄🎁🎅⭐️
+
+Should be less time-consuming than one language a day 🤔
+
+Gonna try to go to the end, this time! 💪🤓
