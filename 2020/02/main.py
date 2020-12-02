18e89d7af41a5406523775808c547b385bebe23e
Tao <tao.galasse@gmail.com>
2020-12-02 12:56:08 +0100

    2020, day 2: part 2

--- 2020/02/main.py
+++ 2020/02/main.py
@@ -25,22 +25,44 @@ def build_dictionary(line):
     }
 
 
-# Return true if a password is valid, following its corporate policy
-def valid_password(dictionary):
+# Return true if a password is valid, following the sled rental place policy
+def valid_password1(dictionary):
     occurrences = dictionary["password"].count(dictionary["letter"])
     return (occurrences >= dictionary["min"] and
             occurrences <= dictionary["max"])
 
 
-# Return the number of valid password in a list of dictionaries
-def number_of_valid_password(dictionaries):
+# Return true if a password is valid,
+# following the Official Toboggan Corporate Policy
+def valid_password2(dictionary):
+    letter1 = dictionary["password"][dictionary["min"] - 1]
+    letter2 = dictionary["password"][dictionary["max"] - 1]
+
+    if letter1 == letter2:
+        return False
+
+    return letter1 == dictionary["letter"] or letter2 == dictionary["letter"]
+
+
+# Call the correct valid_password method following the given policy
+def valid_password(dictionary, policy):
+    if policy == 1:
+        return valid_password1(dictionary)
+    return valid_password2(dictionary)
+
+
+# Return the number of valid passwords in a list of dictionaries,
+# following a policy: 1 for the sled rental place, 2 for the Toboggan Corporate
+def number_of_valid_passwords(dictionaries, policy):
     count = 0
     for dictionary in dictionaries:
-        if valid_password(dictionary):
+        if valid_password(dictionary, policy):
             count += 1
     return count
 
 
-# Puzzle 1
 dictionaries = parse_input("input.txt")
-print(number_of_valid_password(dictionaries))
+# Puzzle 1
+print("Puzzle 1 answer:", number_of_valid_passwords(dictionaries, 1))
+# Puzzle 2
+print("Puzzle 2 answer:", number_of_valid_passwords(dictionaries, 2))
