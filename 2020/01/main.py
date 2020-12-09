99fbb0feb719b4cb47dde56c2be5961b32166a5e
Tao <tao.galasse@gmail.com>
2020-12-09 22:32:11 +0100

    feat: slightly refactor some functions from day 1 to be compatible with day 9

--- 2020/01/main.py
+++ 2020/01/main.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python
 
-# Return values from an input file as an array of integers
-def expenses(filename):
-    expenses = []
-
-    with open(filename, "r") as file:
-        for expense in file.read().splitlines():
-            expenses.append(int(expense))
-
-    return expenses
+# Return values from an input file as a list of integers
+def read_numbers_from_file(filename):
+    return [int(i) for i in open(filename, "r").read().splitlines()]
 
 
-# Find the two values giving a sum equal to 2020 from report
-def find_sum_of_two_values(report):
+# Find two values in a list giving a sum equal to the one given in param
+def find_sum_of_two_values(report, sum):
     for i in range(0, len(report)):
         for j in range(i+1, len(report)):
-            if report[i] + report[j] == 2020:
+            if report[i] + report[j] == sum:
                 return (report[i], report[j])
 
 
-# Find the three values giving a sum equal to 2020 from report
-def find_sum_of_three_values(report):
+# Find three values in a list giving a sum equal to the one given in param
+def find_sum_of_three_values(report, sum):
     for i in range(0, len(report)):
         for j in range(i+1, len(report)):
             for k in range(j+1, len(report)):
-                if report[i] + report[j] + report[k] == 2020:
+                if report[i] + report[j] + report[k] == sum:
                     return (report[i], report[j], report[k])
 
 
 # Puzzle 1
-expense_report = expenses("input.txt")
-expense1, expense2 = find_sum_of_two_values(expense_report)
+expense_report = read_numbers_from_file("input.txt")
+expense1, expense2 = find_sum_of_two_values(expense_report, 2020)
 print("Puzzle 1 answer is:", expense1 * expense2)
 
 # Puzzle 2
-expense1, expense2, expense3 = find_sum_of_three_values(expense_report)
+expense1, expense2, expense3 = find_sum_of_three_values(expense_report, 2020)
 print("Puzzle 2 answer is:", expense1 * expense2 * expense3)
