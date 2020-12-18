2debc1ad1bbf5ce89ed8e34eb8501c948398d635
Tao <tao.galasse@gmail.com>
2020-12-18 02:36:27 +0100

    feat: improve day 17 performances
    10x faster!

--- 2020/17/main.py
+++ 2020/17/main.py
@@ -10,7 +10,7 @@ def parse_input_3d(filename):
     for line in open(filename, 'r').read().splitlines():
         y = 0
         for char in line:
-            grid[(x, y, 0)] = char
+            grid[(x, y, 0)] = 1 if char == '#' else 0
             y += 1
         x += 1
     return grid
@@ -19,27 +19,33 @@ def parse_input_3d(filename):
 # Return the next state of a given 3D grid after a full cycle
 def next_3d_grid(grid):
     next_grid = copy.deepcopy(grid)
-    for x in range(min(k[0] for k in grid.keys()) - 1, max(k[0] for k in grid.keys()) + 2):
-        for y in range(min(k[1] for k in grid.keys()) - 1, max(k[1] for k in grid.keys()) + 2):
-            for z in range(min(k[2] for k in grid.keys()) - 1, max(k[2] for k in grid.keys()) + 2):
-                next_grid[(x, y, z)] = next_3d_cube_state(grid, x, y, z)
+    min_x = min(k[0] for k in grid.keys())
+    max_x = max(k[0] for k in grid.keys())
+    min_y = min(k[1] for k in grid.keys())
+    max_y = max(k[1] for k in grid.keys())
+    min_z = min(k[2] for k in grid.keys())
+    max_z = max(k[2] for k in grid.keys())
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            for z in range(min_z - 1, max_z + 2):
+                next_grid[(x, y, z)] = next_3d_cube(grid, x, y, z)
     return next_grid
 
 
 # Return the next state of a given 3D cube, identified by its coordinates
-def next_3d_cube_state(grid, x, y, z):
+def next_3d_cube(grid, x, y, z):
     active_neighbors = 0
     for xx in range(x-1, x+2):
         for yy in range(y-1, y+2):
             for zz in range(z-1, z+2):
                 if x == xx and y == yy and z == zz:
                     continue
-                if grid.get((xx, yy, zz)) == '#':
+                if grid.get((xx, yy, zz)):
                     active_neighbors += 1
-    if grid.get((x, y, z)) == '#':
-        return '#' if active_neighbors in [2, 3] else '.'
+    if grid.get((x, y, z)):
+        return 1 if active_neighbors in [2, 3] else 0
     else:
-        return '#' if active_neighbors == 3 else '.'
+        return 1 if active_neighbors == 3 else 0
 
 
 # Puzzle 1
@@ -47,7 +53,7 @@ grid = parse_input_3d("input.txt")
 for _ in range(6):
     grid = next_3d_grid(grid)
 print("Number of 3D cubes left in the active state after the sixth cycle:",
-      list(grid.values()).count('#'))
+      list(grid.values()).count(1))
 
 
 ###############################################################################
@@ -60,7 +66,7 @@ def parse_input_4d(filename):
     for line in open(filename, 'r').read().splitlines():
         y = 0
         for char in line:
-            grid[(x, y, 0, 0)] = char
+            grid[(x, y, 0, 0)] = 1 if char == '#' else 0
             y += 1
         x += 1
     return grid
@@ -69,16 +75,24 @@ def parse_input_4d(filename):
 # Return the next state of a given 4D grid after a full cycle
 def next_4d_grid(grid):
     next_grid = copy.deepcopy(grid)
-    for x in range(min(k[0] for k in grid.keys()) - 1, max(k[0] for k in grid.keys()) + 2):
-        for y in range(min(k[1] for k in grid.keys()) - 1, max(k[1] for k in grid.keys()) + 2):
-            for z in range(min(k[2] for k in grid.keys()) - 1, max(k[2] for k in grid.keys()) + 2):
-                for w in range(min(k[3] for k in grid.keys()) - 1, max(k[3] for k in grid.keys()) + 2):
-                    next_grid[(x, y, z, w)] = next_4d_cube_state(grid, x, y, z, w)
+    min_x = min(k[0] for k in grid.keys())
+    max_x = max(k[0] for k in grid.keys())
+    min_y = min(k[1] for k in grid.keys())
+    max_y = max(k[1] for k in grid.keys())
+    min_z = min(k[2] for k in grid.keys())
+    max_z = max(k[2] for k in grid.keys())
+    min_w = min(k[3] for k in grid.keys())
+    max_w = max(k[3] for k in grid.keys())
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            for z in range(min_z - 1, max_z + 2):
+                for w in range(min_w - 1, max_w + 2):
+                    next_grid[(x, y, z, w)] = next_4d_cube(grid, x, y, z, w)
     return next_grid
 
 
 # Return the next state of a given 4D cube, identified by its coordinates
-def next_4d_cube_state(grid, x, y, z, w):
+def next_4d_cube(grid, x, y, z, w):
     active_neighbors = 0
     for xx in range(x-1, x+2):
         for yy in range(y-1, y+2):
@@ -86,12 +100,12 @@ def next_4d_cube_state(grid, x, y, z, w):
                 for ww in range(w-1, w+2):
                     if x == xx and y == yy and z == zz and w == ww:
                         continue
-                    if grid.get((xx, yy, zz, ww)) == '#':
+                    if grid.get((xx, yy, zz, ww)):
                         active_neighbors += 1
-    if grid.get((x, y, z, w)) == '#':
-        return '#' if active_neighbors in [2, 3] else '.'
+    if grid.get((x, y, z, w)):
+        return 1 if active_neighbors in [2, 3] else 0
     else:
-        return '#' if active_neighbors == 3 else '.'
+        return 1 if active_neighbors == 3 else 0
 
 
 # Puzzle 2
@@ -99,4 +113,4 @@ grid = parse_input_4d("input.txt")
 for _ in range(6):
     grid = next_4d_grid(grid)
 print("Number of 4D cubes left in the active state after the sixth cycle:",
-      list(grid.values()).count('#'))
+      list(grid.values()).count(1))
