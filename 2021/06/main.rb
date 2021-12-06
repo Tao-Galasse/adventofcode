b82248fa77a81ffc51cfdb0396bf5166cfb0a979
Tao <tao.galasse@gmail.com>
2021-12-06 13:31:11 +0100

    2021, day 6: add puzzles solutions

---
+++ 2021/06/main.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def iterate
+  timer_to_zero = @timers[0]
+  @timers.transform_keys! { |k| k.zero? ? 8 : k - 1 }
+  @timers[6] += timer_to_zero
+end
+
+@timers = Hash.new(0).merge(File.read('input.txt').split(',').map(&:to_i).group_by(&:itself).transform_values(&:count))
+80.times { iterate }
+puts "part 1: #{@timers.values.sum} lanternfish would be there after 80 days"
+176.times { iterate } # 256 - 80 = 176
+puts "part 2: #{@timers.values.sum} lanternfish would be there after 256 days"
--- 2021/README.md
+++ 2021/README.md
@@ -4,4 +4,4 @@
 
 Here we go again for another year of AoC! 🙌
 
-### Completion : 04/50 ⭐️
+### Completion : 12/50 ⭐️
