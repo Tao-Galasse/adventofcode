9db4ed99277c560df2188959df704ee1a90aaf46
Tao <tao.galasse@gmail.com>
2021-12-10 13:37:43 +0100

    feat: improve day 10 with shared methods for both parts

--- 2021/10/main.rb
+++ 2021/10/main.rb
@@ -21,12 +21,14 @@ AUTOCOMPLETE_CHAR_SCORE = {
   '>' => 4
 }.freeze
 
-# Part 1
-def find_corrupted_lines
-  @lines.map { |line| find_illegal_char(line) }.compact
+def parse_lines
+  @illegal_chars = []
+  @missing_chars = []
+  @lines.each { |line| parse_line(line) }
+  [@illegal_chars, @missing_chars]
 end
 
-def find_illegal_char(line)
+def parse_line(line)
   opened_chunks = []
   line.chars.each do |chunk|
     if CHUNKS.keys.include?(chunk)
@@ -34,33 +36,14 @@ def find_illegal_char(line)
     elsif CHUNKS[opened_chunks[-1]] == chunk # `chunk` is a closing chunk and should match the last opened chunk
       opened_chunks.pop
     else
-      return chunk
+      return @illegal_chars << chunk # corrupted line
     end
   end
-  nil
+  @missing_chars << opened_chunks.reverse.map { |c| CHUNKS[c] } # return missing chars of incomplete line
 end
 
 def syntax_error_score(illegal_chars)
   illegal_chars.map { |c| ILLEGAL_CHAR_SCORE[c] }.sum
-end
-
-# Part 2
-def find_incomplete_lines
-  @lines.map { |line| find_missing_chars(line) }.compact
-end
-
-def find_missing_chars(line)
-  opened_chunks = []
-  line.chars.each do |chunk|
-    if CHUNKS.keys.include?(chunk)
-      opened_chunks << chunk
-    elsif CHUNKS[opened_chunks[-1]] == chunk # `chunk` is a closing chunk and should match the last opened chunk
-      opened_chunks.pop
-    else
-      return nil # this is a corrupted line ; we ignore it
-    end
-  end
-  opened_chunks.reverse.map { |c| CHUNKS[c] }
 end
 
 def middle_score(missing_chars)
@@ -73,7 +56,6 @@ def autocomplete_line_score(chars)
 end
 
 @lines = File.readlines('input.txt', chomp: true)
-illegal_chars = find_corrupted_lines
+illegal_chars, missing_chars = parse_lines
 puts "part 1: syntax error score = #{syntax_error_score(illegal_chars)}"
-missing_chars = find_incomplete_lines
 puts "part 2: middle score = #{middle_score(missing_chars)}"
