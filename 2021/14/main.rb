29dc7ea4ba5965f97804bc97fc584e557d01edf1
Tao <tao.galasse@gmail.com>
2021-12-15 04:36:17 +0100

    2021, day 14: refactor code to get puzzle 2 solution

--- 2021/14/main.rb
+++ 2021/14/main.rb
@@ -1,43 +1,26 @@
 # frozen_string_literal: true
 
-# def process_step(polymer)
-#   next_polymer = polymer[0]
-#   polymer.chars.each_cons(2) { |elem1, elem2| next_polymer += "#{@rules["#{elem1}#{elem2}"]}#{elem2}" }
-#   next_polymer
-# end
-
-# def process_steps(polymer, steps)
-#   steps.times.map { polymer = process_step(polymer) }.last
-# end
-
-# def difference(polymer)
-#   occurrences = polymer.chars.tally.values
-#   occurrences.max - occurrences.min
-# end
-
-def process_step(elem1, elem2, remaining_steps)
-  return if remaining_steps.zero?
-
-  middle_letter = @rules["#{elem1}#{elem2}"]
-  @occurrences[middle_letter] += 1
-  process_step(elem1, middle_letter, remaining_steps - 1)
-  process_step(middle_letter, elem2, remaining_steps - 1)
+def process_step
+  next_pairs = Hash.new(0)
+  @pairs_count.each do |pair, count|
+    middle_letter = @rules[pair]
+    next_pairs["#{pair[0]}#{middle_letter}"] += count
+    next_pairs["#{middle_letter}#{pair[1]}"] += count
+    @letters_count[middle_letter] += count
+  end
+  @pairs_count = next_pairs
 end
 
-def process_steps(polymer, steps)
-  polymer.chars.each { |c| @occurrences[c] += 1 }
-  polymer.chars.each_cons(2) { |elem1, elem2| process_step(elem1, elem2, steps) }
-end
-
-def difference
-  @occurrences.values.max - @occurrences.values.min
+def max_min_difference
+  @letters_count.values.max - @letters_count.values.min
 end
 
 polymer, @rules = File.read('input.txt').split("\n\n")
 @rules = @rules.split("\n").map { |r| r.split(' -> ') }.to_h
+@pairs_count = Hash.new(0).merge(polymer.chars.each_cons(2).map { |a, b| "#{a}#{b}" }.tally)
+@letters_count = Hash.new(0).merge(polymer.chars.tally)
 
-@occurrences = Hash.new(0)
-process_steps(polymer, 10)
-puts "part 1: difference between most and least common elements after 10 steps = #{difference}"
-process_steps(polymer, 40)
-puts "part 2: difference between most and least common elements after 40 steps = #{difference}"
+10.times { process_step }
+puts "part 1: difference between most and least common elements after 10 steps = #{max_min_difference}"
+30.times { process_step } # 40 - 10 = 30
+puts "part 2: difference between most and least common elements after 40 steps = #{max_min_difference}"
