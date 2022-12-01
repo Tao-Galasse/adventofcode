f5dd875ecb0a5f9bcf844f1611d8a056eb603bb3
Tao <tao.galasse@gmail.com>
2022-12-01 20:19:37 +0100

    refactor: use better input parsing

--- 2022/01/main.rb
+++ 2022/01/main.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
-lines = File.readlines('input.txt', chomp: true)
-
-calories = []
-total = 0
-
-lines.each do |line|
-  if line.empty?
-    calories << total
-    total = 0
-  else
-    total += line.to_i
-  end
-end
+calories = File.read('input.txt').split("\n\n").map { |values| values.split("\n").map(&:to_i).sum }
 
 puts "part 1: The elf carrying the most calories has #{calories.max} calories"
-puts "part 2: The three elves carrying the most calories has a sum of #{calories.max(3).sum} calories"
+puts "part 2: The three elves carrying the most calories have a sum of #{calories.max(3).sum} calories"
