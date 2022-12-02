f48f0abe050e51e8c583e2c6246e912523297bfb
Tao <tao.galasse@gmail.com>
2022-12-02 12:27:48 +0100

    refactor: use `inject(0)` instead of itering with an `each` and incrementing a variable

--- 2022/02/main.rb
+++ 2022/02/main.rb
@@ -9,33 +9,24 @@ SCORES = { 'X' => 1, 'Y' => 2, 'Z' => 3 }.freeze
 
 # The second param of a round is what I have to play : X = Rock, Y = Paper, Z = Scissors
 def play_part1(rounds)
-  score = 0
-
-  rounds.each do |opponent, me|
+  rounds.inject(0) do |score, (opponent, me)|
     case me
     when WIN[opponent] then score += 6
     when DRAW[opponent] then score += 3
     end
-
-    score += SCORES[me]
+    score + SCORES[me]
   end
-
-  score
 end
 
 # The second param of a round is how it must end for me : X = lose, Y = draw, Z = win
 def play_part2(rounds)
-  score = 0
-
-  rounds.each do |opponent, result|
+  rounds.inject(0) do |score, (opponent, result)|
     case result
-    when 'X' then score += SCORES[LOSE[opponent]]
-    when 'Y' then score += SCORES[DRAW[opponent]] + 3
-    when 'Z' then score += SCORES[WIN[opponent]] + 6
+    when 'X' then score + SCORES[LOSE[opponent]]
+    when 'Y' then score + SCORES[DRAW[opponent]] + 3
+    when 'Z' then score + SCORES[WIN[opponent]] + 6
     end
   end
-
-  score
 end
 
 rounds = File.readlines('input.txt', chomp: true).map(&:split)
